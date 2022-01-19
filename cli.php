e5b6f407a492dfe2523db8c5ecb3cb7232edb74d
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-01-19 12:22:53 +0100

    making difference beetween commands and keywords

--- cli.php
+++ cli.php
@@ -1,12 +1,12 @@
 <?php
 function extend($text) {
-    // get the commands from the json file
-    // the file is a json object with 3 properties:
-    // - commands: an array of commands
-    // - regex: an array of regexes that are always good
-    // - interfaces: an array of interface names that are used with n/n at the end
+    # get the commands from the json file
+    # the file is a json object with 3 properties:
+    # - commands: an array of commands
+    # - regex: an array of regexes that are always good
+    # - interfaces: an array of interface names that are used with n/n at the end
     $commands = json_decode(file_get_contents('commands.json'), true);
-    $lines = preg_split("/\r\n|\n|\r/", strtolower($text)); // convert to lowercase and split into lines
+    $lines = preg_split("/\r\n|\n|\r/", strtolower($text)); # convert to lowercase and split into lines
     for ($i=0; $i < count($lines); $i++) { 
         $lines[$i] = line($lines[$i], $commands);
     }
@@ -14,31 +14,20 @@ function extend($text) {
 }
 
 function line($text, $commands) {
-    $words = explode(" ", $text); // Split text into words
-    for ($i=0; $i < count($words); $i++) { 
+    $words = explode(" ", $text); # Split text into words
+    if (count($words) > 0) {
+        $words[0] = command($words[0], $commands);
+    }
+    for ($i=1; $i < count($words); $i++) { 
         $words[$i] = word(str_replace(" ", "", $words[$i]), $commands);
     }
     return implode(" ", $words);
 }
 
-function word($text, $commands) { // for each word
+function command($text, $commands) {
     $l = strlen($text);
     $r = "";
-    // check if contains a number/number
-    if (preg_match("/([0-9][0-9]?\/[0-9][0-9]?)/", $text)) {
-        // get index of number/number and divide the word in [interfaceName] and [number/number]
-        $p = preg_match_all("/([0-9][0-9]?\/[0-9][0-9]?)/", $text);
-        $sub = substr($text, 0, $p);
-        $sc = strlen($sub);
-        $num = substr($text, $p + 1, $l-$sc);
-        // Check the interface that start with the first part of the word
-        for ($i=0; $i < count($commands["interfaces"]); $i++) { 
-            if (substr($commands["interfaces"][$i], 0, $sc) == $sub) {
-                return $commands["interfaces"][$i] . $num;
-            }
-        }
-    }
-    // for each command this checks if a command start with the word, if more than one command start with the word, it will return the word
+    # for each command this checks if a commands start with the word, if more than one commands start with the word, it will return the word
     for ($i=0; $i < count($commands["commands"]); $i++) {
         if ($commands["commands"][$i] == $text) {
             return $text;
@@ -50,16 +39,64 @@ function word($text, $commands) { // for each word
             }
         }
     }
+    # if no command is found, return the word with error, else the command
+    if ($r == "") {
+        $spl = str_split($text)[0];
+        if ($spl[0] == "*" && $spl[strlen($spl) - 1] == "*") { # Check if the word is already checked as an error
+            return $text;
+        }
+        return "*" . $text . "*"; 
+    } else {
+        return $r;
+    }
+}
 
-    // for each regex if the word matches the regex, it will return the word
+function word($text, $commands) { # for each word
+    $l = strlen($text);
+    $r = "";
+    # check if contains a number/number
+    if (preg_match("/([0-9][0-9]?\/[0-9][0-9]?)/", $text)) {
+        # get index of number/number and divide the word in [interfaceName] and [number/number]
+        $p = -1;
+        if(preg_match("/([0-9][0-9]?\/[0-9][0-9]?)/", $text, $matches, PREG_OFFSET_CAPTURE)) {
+            $p = $matches[0][1];
+        }
+        $sub = substr($text, 0, $p);
+        $sc = strlen($sub);
+        $num = substr($text, $p, $l-$sc);
+        # Check the interface that start with the first part of the word
+        for ($i=0; $i < count($commands["interfaces"]); $i++) { 
+            if (substr($commands["interfaces"][$i], 0, $sc) == $sub) {
+                return $commands["interfaces"][$i] . $num;
+            }
+        }
+    }
+    # for each command this checks if a keyword start with the word, if more than one keyword start with the word, it will return the word
+    for ($i=0; $i < count($commands["keywords"]); $i++) {
+        if ($commands["keywords"][$i] == $text) {
+            return $text;
+        } else if(substr($commands["keywords"][$i], 0, $l) == $text) {
+            if ($r == "") {
+                $r = $commands["keywords"][$i];
+            } else {
+                return $text;
+            }
+        }
+    }
+
+    # for each regex if the word matches the regex, it will return the word
     for ($i=0; $i < count($commands["regex"]); $i++) {
         if(preg_match($commands["regex"][$i], $text)) {
             return $text;
         }
     }
 
-    // if no command is found, return the word with error, else the command
+    # if no command is found, return the word with error, else the command
     if ($r == "") {
+        $spl = str_split($text)[0];
+        if ($spl[0] == "*" && $spl[strlen($spl) - 1] == "*") { # Check if the word is already checked as an error
+            return $text;
+        }
         return "*" . $text . "*"; 
     } else {
         return $r;
--- index.php
+++ index.php
@@ -5,6 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cisco Cli</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            width: 100vw;
+            height: 100vh;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -14,7 +27,7 @@
             $r = extend($_POST["command"]);
         }
     ?>
-    <form action="/" method="post">
+    <form action="" method="post">
         <textarea name="command" cols="30" rows="10"><?php echo $r;?></textarea>
         <input type="submit" value="Submit">
     </form>
