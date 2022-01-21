a30f274591ec2422f70e134a52b9a4bf9e550047
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2022-01-21 15:55:44 +0100

    made more accurate the interface regex

--- cli.php
+++ cli.php
@@ -75,7 +75,7 @@ function word($text, $commands) { # for each word
     $l = strlen($text);
     $r = "";
     # check if contains a number/number
-    if (preg_match("/([0-9][0-9]?\/[0-9][0-9]?)/", $text)) {
+    if (preg_match("/^([a-z]?){25}([0-9][0-9]?\/[0-9][0-9]?)$/", $text)) {
         # get index of number/number and divide the word in [interfaceName] and [number/number]
         $p = -1;
         if(preg_match("/([0-9][0-9]?\/[0-9][0-9]?)/", $text, $matches, PREG_OFFSET_CAPTURE)) {
